4ea214cfa0e38e760109136806b0c430328844f3
AscarTec <mr.askarlawyer@gmail.com>
2026-05-12 09:28:36 +0300

    Update config.jsx

--- avocat-frontend/src/config/config.jsx
+++ avocat-frontend/src/config/config.jsx
@@ -1,6 +1,6 @@
 const API_CONFIG = {
  //  baseURL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8000',
-  baseURL: 'https://avocat-backend-lrmi-copy-production.up.railway.app',
+  baseURL: 'https://avocat-backend-production.up.railway.app',
 };
 
 export default API_CONFIG;
